c9bd509992eca50596eeb539ce628bc37b7f417a
kellynh541_lpsk12 <kellynh541@lpsk12.org>
2023-09-26 10:01:58 -0600

    kdjasbfa

--- PIP.java
+++ PIP.java
@@ -6,23 +6,37 @@ public class PIP {
 
         for(int i = 0; 0 < input.length(); i+2) {
             String temp = substring(i, i+1);
+            int size = postfix.size;
             if(temp.equals("+")) {
 
                 int new = postfix.pop() + postfix.pop();
                 postfix.push(new);
+                if (size < 2){
+                    throw new IllegalArgumentException;
+                }
 
             } else if (temp.equals("-")) {
 
-                if(input.susbtring(i+1,i+2).equals(" "))
+                if(input.susbtring(i+1,i+2).equals(" ")){
+                    int a = postfix.pop();
+                    int b = postfix.pop();
+                    postfix.push(b-a);
+                    f (size < 2){
+                        throw new IllegalArgumentException;
 
-                int a = postfix.pop();
-                int b = postfix.pop();
-                postfix.push(b-a);
+                    }
+                }
+                int value = Integer.parseInt(input.susbtring(i+1,i+2));
+                postfix.push(value);
+                i++;
 
             } else if (temp.equals("*")) {
 
                 int new = postfix.pop() * postfix.pop();
                 postfix.push(new);
+                f (size < 2){
+                    throw new IllegalArgumentException;
+                }
 
             } else if (temp.equals("/")) {
 
@@ -30,14 +44,23 @@ public class PIP {
                 int b = postfix.pop();
                 postfix.push(b/a);
 
-            } else {
+                f (size < 2){
+                    throw new IllegalArgumentException;
 
+            } else {
+                try{Integer.parseInt;
+                } catch (NumberFormatException nfe){
+                    System.out.print ("Please only use numbers and operators!");
+                }
                 int value = Integer.parseInt(temp);
 
             }
 
         }
-
+    if(postfix.size != 1){
+        throw new IllegalArgumentException;
+        }
+    return postfix.push();
     }
 
 }
