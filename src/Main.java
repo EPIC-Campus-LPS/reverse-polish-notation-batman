bf36b51f7fe182cab400af43dc49e2d1d06259a8
mariev718_lpsk12 <mariev718@lpsk12.org>
2023-09-26 10:27:07 -0600

    PIP class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.print(PIP.evaluatePostfix("2 3 + 5 /"));
+
     }
 }
---
+++ src/PIP.java
@@ -0,0 +1,91 @@
+import java.util.*;
+public class PIP {
+
+    static String evaluatePostfix(String input) {
+
+        Stack<Integer> postfix = new Stack<>();
+
+        for (int i = 0; 0 < input.length() - 1; i ++) {
+
+            String temp = input.substring(i, i + 1);
+
+            int size = postfix.size();
+
+            if(temp.equals(" ")) {
+
+                i++;
+
+            } else if (temp.equals("+")) {
+
+                if (size < 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                int newValue = postfix.pop() + postfix.pop();
+
+                postfix.push(newValue);
+
+            } else if (temp.equals("-")) {
+
+                if (size < 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                if (input.substring(i + 1, i + 2).equals(" ")) {
+                    int a = postfix.pop();
+                    int b = postfix.pop();
+                    postfix.push(b - a);
+
+                }
+
+                int value = Integer.parseInt(input.substring(i + 1, i + 2));
+                postfix.push(value);
+                i++;
+
+            } else if (temp.equals("*")) {
+
+                if (size < 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                int newValue = postfix.pop() * postfix.pop();
+                postfix.push(newValue);
+
+
+            } else if (temp.equals("/")) {
+
+                if (size < 2) {
+                    throw new IllegalArgumentException();
+                }
+
+                int a = postfix.pop();
+                int b = postfix.pop();
+
+                postfix.push(b / a);
+
+            } else {
+
+                try {
+                    Integer.parseInt(temp);
+                } catch (NumberFormatException nfe) {
+                    System.out.print("Please only use numbers and operators!");
+                }
+
+                int value = Integer.parseInt(temp);
+                postfix.push(value);
+
+            }
+
+        }
+
+        if (postfix.size() != 1) {
+
+            throw new IllegalArgumentException();
+
+        }
+
+        return String.valueOf(postfix.pop());
+
+    }
+
+}
